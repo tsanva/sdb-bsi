86835964cdb9c2df6c38052768fc9be74b523de8
hafidzulFikri <19523033@students.uii.ac.id>
2022-08-24 07:35:36 +0800

    Update sbd.sql

--- database/sbd.sql
+++ database/sbd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 23, 2022 at 07:00 AM
+-- Generation Time: Aug 24, 2022 at 01:34 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -70,6 +70,25 @@ CREATE TABLE `penyewaan` (
   `status_bayar` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `petugas`
+--
+
+CREATE TABLE `petugas` (
+  `ID_petugas` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `petugas`
+--
+
+INSERT INTO `petugas` (`ID_petugas`, `username`, `password`) VALUES
+(1, 'admin', 'admin');
+
 --
 -- Indexes for dumped tables
 --
@@ -93,6 +112,12 @@ ALTER TABLE `penyewaan`
   ADD PRIMARY KEY (`ID_penyewaan`);
 
 --
+-- Indexes for table `petugas`
+--
+ALTER TABLE `petugas`
+  ADD PRIMARY KEY (`ID_petugas`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -113,6 +138,12 @@ ALTER TABLE `nasabah`
 --
 ALTER TABLE `penyewaan`
   MODIFY `ID_penyewaan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `petugas`
+--
+ALTER TABLE `petugas`
+  MODIFY `ID_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
